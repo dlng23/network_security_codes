e1f0a03a4fc1a913144ec6e2efe192381eea0e06
dlng23 <1myungjin@ewhain.net>
2025-04-06 21:55:02 +0900

    PCAP Programming

---
+++ PCAP_Programming/pcap_assignment.c
@@ -0,0 +1,121 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+/* Ethernet header */
+struct ethheader {
+    u_char  ether_dhost[6]; /* destination host address */
+    u_char  ether_shost[6]; /* source host address */
+    u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
+  };
+
+/* IP Header */
+struct ipheader {
+    unsigned char      iph_ihl:4, //IP header length
+                       iph_ver:4; //IP version
+    unsigned char      iph_tos; //Type of service
+    unsigned short int iph_len; //IP Packet length (data + header)
+    unsigned short int iph_ident; //Identification
+    unsigned short int iph_flag:3, //Fragmentation flags
+                       iph_offset:13; //Flags offset
+    unsigned char      iph_ttl; //Time to Live
+    unsigned char      iph_protocol; //Protocol type
+    unsigned short int iph_chksum; //IP datagram checksum
+    struct  in_addr    iph_sourceip; //Source IP address
+    struct  in_addr    iph_destip;   //Destination IP address
+  };
+
+/* TCP Header */
+struct tcpheader {
+    u_short tcp_sport;               /* source port */
+    u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
+    u_char  tcp_flags;
+    u_short tcp_win;                 /* window */
+    u_short tcp_sum;                 /* checksum */
+    u_short tcp_urp;                 /* urgent pointer */
+};
+
+#define ETHERNET_SIZE 14
+#define MAX_PAYLOAD_LEN 32
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+    const u_char *packet)
+{
+    struct ethheader *eth = (struct ethheader *)packet;
+
+    if (ntohs(eth->ether_type) != 0x0800) return; // not IP
+
+    struct ipheader * ip = (struct ipheader *)
+                           (packet + sizeof(struct ethheader));
+                    
+    if (ip->iph_protocol != IPPROTO_TCP) return; // not TCP
+
+    int ip_header_len = ip->iph_ihl * 4;
+
+    struct tcpheader *tcp = (struct tcpheader *)(packet + ETHERNET_SIZE + ip_header_len);
+    int tcp_header_len = ((tcp->tcp_offx2 & 0xf0) >> 4) * 4;
+
+    const u_char *payload = packet + ETHERNET_SIZE + ip_header_len + tcp_header_len;
+    int payload_len = header->len - (ETHERNET_SIZE + ip_header_len + tcp_header_len);
+
+    printf("\n=== TCP Packet Captured ===\n");
+
+    // MAC 출력
+    printf("Src MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2],
+           eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
+
+    printf("Dst MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2],
+           eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]);
+
+    // IP & Port 출력
+    printf("Src IP: %s\n", inet_ntoa(ip->iph_sourceip));
+    printf("Dst IP: %s\n", inet_ntoa(ip->iph_destip));
+    printf("Src Port: %d\n", ntohs(tcp->tcp_sport));
+    printf("Dst Port: %d\n", ntohs(tcp->tcp_dport));
+
+    // Payload 출력
+    if (payload_len > 0) {
+        printf("Message (%d bytes): ", payload_len < MAX_PAYLOAD_LEN ? payload_len : MAX_PAYLOAD_LEN);
+        for (int i = 0; i < payload_len && i < MAX_PAYLOAD_LEN; i++) {
+            printf("%c", isprint(payload[i]) ? payload[i] : '.');
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    char *dev = "enp0s3";  
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
+    struct bpf_program fp;
+    char filter_exp[] = "tcp";
+    bpf_u_int32 net;
+
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "Could not open device %s: %s\n", dev, errbuf);
+        return 1;
+    }
+
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return 1;
+    }
+
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        return 1;
+    }
+
+    printf("Listening on %s for TCP packets...\n", dev);
+    pcap_loop(handle, 0, got_packet, NULL);
+    pcap_close(handle);
+
+    return 0;
+}
